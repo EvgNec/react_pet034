2c83bd60508ce918917e980976cfd9a9861f948d
Evgeniy <evgnec@gmail.com>
2025-06-15 17:11:13 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+
+  };
+
+  componentDidMount(){
+
+  }
+  render() {
+    const {  } = this.state;
+    return (
+      <>
+
+      </>
+    );
+  }
+}
