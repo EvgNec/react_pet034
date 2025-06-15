9c8c5e720d24191000a591098774b84d9c8e9b4e
Evgeniy <evgnec@gmail.com>
2025-06-15 17:34:04 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 
 export class App extends Component {
   state = {
-
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: ''
   };
 
   componentDidMount(){
-
   }
+  
+  handleChange = (e) => {
+    this.setState({ name: e.target.value });
+  }
+
   render() {
-    // const {  } = this.state;
+    const { contacts, name } = this.state;
     return (
       <>
-Test
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            placeholder="Name"
+            onChange={this.handleChange}
+            value={name}
+          />
+        </label>
+
+        <button type="submit">Add</button>
+      </form>
       </>
     );
   }
